10ba2486c8d4599d901b98aea0a120be76a3f6f9
Koshukey <koshuke0217@icloud.com>
2018-04-08 12:23:21 +0900

    Rubyで遊んでみる by 標準ライブラリ

--- play_ruby.rb
+++ play_ruby.rb
@@ -1,6 +1,13 @@
 require 'json'
 require 'pry'
 require 'securerandom'
+require 'rexml/document'
+require 'prime'
+#素数
+require 'abbrev'
+#与えられた文字列の短縮形を生成してくれる
+require 'pp'
+#出力結果を自動で改行してくれる
 
 ruby_data = {
   "num" => 1,
@@ -21,3 +28,16 @@ json_result = ruby_result.to_json
 puts  json_result
 
 puts SecureRandom.hex(8)
+#securerandomライブラリでランダムな文字列を作成
+
+Prime.each(50) do |prime|
+  p prime
+end
+#Primeクラスは素数全体を表す
+
+p Prime.instance
+
+prime_instance = Prime.instance
+p Prime.prime_division(12)
+
+pp Abbrev.abbrev(["ruby"])
